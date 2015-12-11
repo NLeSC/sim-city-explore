8924458ac198b2a3dbf48a8a7398ce0fb9d0db45
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-12-11 15:42:15 +0100

    Documented parameter parsing

--- simcityexplore/parameter.py
+++ simcityexplore/parameter.py
@@ -14,12 +14,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+'''
+Specify a parameter space with a JSON schema. A parameter set can be validated
+according to this space and the space can be sampled.
+'''
+
 import math
 import jsonschema
 import types
 
 
 def parse_parameters(parameters, schema):
+    '''
+    Validates given parameters according to a JSON schema
+
+    Parameters
+    ----------
+    parameters: dict
+        a deep dict of values, where values may be simple types, dicts or lists
+    schema: dict
+        an object conforming to JSON schema syntax and semantics. Parameters
+        will be checked according to this schema.
+
+    Raises
+    ------
+    ValueError: if the parametrs do not conform to the schema
+    EnvironmentError: if the schema is not a valid JSON schema
+    '''
     try:
         jsonschema.validate(parameters, schema)
     except jsonschema.SchemaError as ex:
@@ -29,24 +50,53 @@ def parse_parameters(parameters, schema):
 
 
 def num_samples_object(chooser, schema):
+    '''
+    Given a Chooser object, run a deep scan of number of samples required for a
+    single parameter setting of a schema. Generally, each simple type takes one
+    sample.
+    '''
     return sum(chooser._number_of_samples(s)
                for s in schema['properties'].values())
 
 def num_samples_array(chooser, schema):
+    '''
+    Given a Chooser object, run a deep scan of number of samples required for a
+    single parameter setting of an array variable in a JSON schema. This
+    function returns the lowest possible value (minimum array length).
+    '''
     return (chooser._number_of_samples(schema['items']) *
             schema.get('minItems', 0))
 
 
 def num_samples_ref(chooser, schema):
+    '''
+    Given a Chooser object, determine the number of samples required for a
+    single parameter setting of a referenced object in a JSON schema.
+    '''
     return chooser._number_of_samples(
         chooser.resolver.resolve(schema['$ref'])[1])
 
 
 def num_samples_allOf(chooser, schema):
+    '''
+    Given a Chooser object, determine the number of samples required for a
+    single parameter setting of an allOf aggregation in a JSON schema.
+    '''
     return sum(chooser._number_of_samples(s) for s in schema['allOf'])
 
 
 def choose_object(chooser, schema, value, idx):
+    '''
+    Given a Chooser object, create a parameter setting from an object in a JSON
+    schema. It takes values from the value array, starting from index idx.
+
+    Parameters
+    ----------
+    chooser: Chooser
+    schema: dict of a JSON schema with `properties` key
+    value: an array of sampled values
+    idx: index in the array to start using values of
+    '''
     ordered = sorted(schema['properties'].keys())
 
     props = {}
@@ -57,6 +107,17 @@ def choose_object(chooser, schema, value, idx):
 
 
 def choose_array(chooser, schema, value, idx):
+    '''
+    Given a Chooser object, create a parameter setting from an array in a JSON
+    schema. It takes values from the value array, starting from index idx.
+
+    Parameters
+    ----------
+    chooser: Chooser
+    schema: dict of a JSON schema with `items` key
+    value: an array of sampled values
+    idx: index in the array to start using values of
+    '''
     arr = []
     for i in xrange(schema.get('minItems', 0)):
         v, idx = chooser._choose(schema['items'], value, idx)
@@ -65,14 +126,49 @@ def choose_array(chooser, schema, value, idx):
 
 
 def choose_enum(chooser, schema, value, idx):
+    '''
+    Given a Chooser object, create a parameter setting from an enum in a JSON
+    schema. It takes values from the value array, starting from index idx.
+
+    Parameters
+    ----------
+    chooser: Chooser
+    schema: dict of a JSON schema with `enum` key
+    value: an array of sampled values
+    idx: index in the array to start using values of
+    '''
     return schema['enum'][int(value * len(schema))], idx + 1
 
 
 def choose_ref(chooser, schema, value, idx):
+    '''
+    Given a Chooser object, create a parameter setting from an referenced
+    object in a JSON schema. It takes values from the value array, starting
+    from index idx.
+
+    Parameters
+    ----------
+    chooser: Chooser
+    schema: dict of a JSON schema with `$ref` key
+    value: an array of sampled values
+    idx: index in the array to start using values of
+    '''
     return chooser._choose(chooser.resolver.resolve(schema['$ref'])[1], value, idx)
 
 
 def choose_allOf(chooser, schema, value, idx):
+    '''
+    Given a Chooser object, create a parameter setting from an allOf
+    aggregation in a JSON schema. It takes values from the value array,
+    starting from index idx.
+
+    Parameters
+    ----------
+    chooser: Chooser
+    schema: dict of a JSON schema with `allOf` key
+    value: an array of sampled values
+    idx: index in the array to start using values of
+    '''
     props = {}
     for s in schema['allOf']:
         ret, idx = chooser._choose(s, value, idx)
@@ -81,6 +177,20 @@ def choose_allOf(chooser, schema, value, idx):
 
 
 def choose_number(chooser, schema, value, idx):
+    '''
+    Given a Chooser object, create a single number from a number setting in a
+    JSON schema. It takes values from the value array, starting from index idx.
+
+    Values is a float ranging from [minimum, maximum). If minimum or maximum
+    are not defined, they are taken as infinity.
+
+    Parameters
+    ----------
+    chooser: Chooser
+    schema: dict of a JSON schema with optional `minimum` and `maximum` keys
+    value: an array of sampled values
+    idx: index in the array to start using values of
+    '''
     x = value[idx]
     xmin = schema.get('minimum')
     xmax = schema.get('maximum')
@@ -103,10 +213,39 @@ def choose_number(chooser, schema, value, idx):
 
 
 def choose_integer(chooser, schema, value, idx):
-    return int(chooser.for_type('number')(chooser, schema, value, idx)[0]), idx + 1
+    '''
+    Given a Chooser object, create a single number from a number setting in a
+    JSON schema. It takes values from the value array, starting from index idx.
+
+    Values is a integer ranging from [minimum, maximum]. If minimum or maximum
+    are not defined, they are taken as infinity.
+
+    Parameters
+    ----------
+    chooser: Chooser
+    schema: dict of a JSON schema with optional `minimum` and `maximum` keys
+    value: an array of sampled values
+    idx: index in the array to start using values of
+    '''
+    return (int(chooser.for_type('number')(chooser, schema, value, idx)[0]),
+            idx + 1)
 
 
 def choose_string(chooser, schema, value, idx):
+    '''
+    Given a Chooser object, create a single number from a number setting in a
+    JSON schema. It takes values from the value array, starting from index idx.
+
+    Values is a string containing characters [0-9a-f] with length `minLength`
+    (defaults to `maxLength` if it is smaller than 10, to 10 otherwise)
+
+    Parameters
+    ----------
+    chooser: Chooser
+    schema: dict of a JSON schema with optional `minLength` and `maxLength` keys
+    value: an array of sampled values
+    idx: index in the array to start using values of
+    '''
     if isinstance(value[idx], types.StringTypes):
         return value[idx], idx + 1
 
@@ -175,6 +314,10 @@ class Chooser(object):
         self.num_params.update(num_parameters)
 
     def _choose(self, schema, value, idx):
+        '''
+        Given a schema, choose the value of each property in the schema
+        based on random sample in value. Use Chooser.choose instead.
+        '''
         for prop in self.choosers['properties']:
             if prop in schema:
                 chooser = self.for_property(prop)
@@ -187,13 +330,28 @@ class Chooser(object):
         raise ValueError('schema {0} cannot be chosen'.format(schema))
 
     def choose(self, value):
-        ''' '''
+        '''
+        Choose the value of each property in the JSON schema
+        based on random sample in value. The length of the random sample should
+        be at least the output of Chooser.num_parameters.
+        '''
         return self._choose(self.schema, value, 0)[0]
 
     def num_parameters(self):
+        '''
+        Determine the number of free parameters in the JSON schema.
+
+        For arrays and strings, the minimum length is taken to determine this
+        number.
+        '''
         return self._number_of_samples(self.schema)
 
     def _number_of_samples(self, schema):
+        '''
+        Determine the number of free parameters in the JSON schema.
+
+        Use num_parameters instead.
+        '''
         for prop in self.num_params:
             if prop in schema:
                 return self.num_params[prop](self, schema)
@@ -201,8 +359,9 @@ class Chooser(object):
             return 1
 
     def for_type(self, typename):
+        ''' Select the chooser for given JSON schema type. '''
         return self.choosers['types'][typename]
 
     def for_property(self, prop):
+        ''' Select the chooser for given JSON schema property. '''
         return self.choosers['properties'][prop]
-
